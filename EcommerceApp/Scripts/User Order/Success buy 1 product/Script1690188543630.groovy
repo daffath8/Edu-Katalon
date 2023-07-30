9f570dc26420b5cbd34696cebc900d075902908b
DESKTOP-T5MELBJ\daffa <daffathh0800@gmail.com>
2023-07-30 20:38:20 +0700

    Latihan Katalon

---
+++ EcommerceApp/Scripts/User Order/Success buy 1 product/Script1690188543630.groovy
@@ -0,0 +1,77 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+if (Mobile.verifyElementExist(findTestObject('Object Repository/HomePage/banner_PRODUCTS'),0)) {
+	Mobile.tap(findTestObject('HomePage/changeView_icon'), 0)
+	
+	Mobile.tap(findTestObject('Order/ADD_TO_CART_btn'), 0)
+	
+	Mobile.tap(findTestObject('Order/Cart_btn'), 0)
+	
+	Mobile.verifyElementText(findTestObject('Order/VerifyProduct(Backpack)'), 'Sauce Labs Backpack')
+	
+	Mobile.tap(findTestObject('Order/Checkout_btn'), 0)
+	
+	Mobile.setText(findTestObject('Order/FirstName_field'), 'Daffa', 0)
+	
+	Mobile.setText(findTestObject('Order/LastName_btn'), 'Firdausi', 0)
+	
+	Mobile.setText(findTestObject('Order/PostalCode_field'), '16425', 0)
+	
+	Mobile.tap(findTestObject('Order/Continue_btn'), 0)
+	
+	Mobile.verifyElementText(findTestObject('Order/VerifyCheckoutOverview'), 'CHECKOUT: OVERVIEW')
+	
+	Mobile.verifyElementText(findTestObject('Order/VerifyProduct(Backpack)'), 'Sauce Labs Backpack')
+	
+	Mobile.scrollToText('FINISH', FailureHandling.STOP_ON_FAILURE)
+	
+	Mobile.tap(findTestObject('Order/FinishBtn'), 0)
+	
+	Mobile.verifyElementText(findTestObject('Order/alertSuccessOrder'), 'THANK YOU FOR YOU ORDER')
+} else {
+WebUI.callTestCase(findTestCase('Login Page/Success login'), [:], FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('Order/ADD_TO_CART_btn'), 0)
+
+Mobile.tap(findTestObject('Order/Cart_btn'), 0)
+
+Mobile.verifyElementText(findTestObject('Order/VerifyProduct(Backpack)'), 'Sauce Labs Backpack')
+
+Mobile.tap(findTestObject('Order/Checkout_btn'), 0)
+
+Mobile.setText(findTestObject('Order/FirstName_field'), 'Daffa', 0)
+
+Mobile.setText(findTestObject('Order/LastName_btn'), 'Firdausi', 0)
+
+Mobile.setText(findTestObject('Order/PostalCode_field'), '16425', 0)
+
+Mobile.tap(findTestObject('Order/Continue_btn'), 0)
+
+Mobile.verifyElementText(findTestObject('Order/VerifyCheckoutOverview'), 'CHECKOUT: OVERVIEW')
+
+Mobile.verifyElementText(findTestObject('Order/VerifyProduct(Backpack)'), 'Sauce Labs Backpack')
+
+Mobile.scrollToText('FINISH', FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('Order/FinishBtn'), 0)
+
+Mobile.verifyElementText(findTestObject('Order/alertSuccessOrder'), 'THANK YOU FOR YOU ORDER')
+}
+
+
+
---
+++ EcommerceApp/Test Suites/Testing E2E.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Testing E2E</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>2cedf37b-cfc4-4a6b-b2f1-ebc2e135ece2</testSuiteGuid>
+   <testCaseLink>
+      <guid>d8309dfb-2b2c-47b3-94b2-1cba190a5fb5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Page/Failed Login Password isEmpty</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8e39fa5f-2790-4642-9d97-c2ff66cdb516</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Page/Failed Login Username isEmpty</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bcbdcd70-0ea1-41e5-a1b1-a2edbd402cdc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Page/Failed login using false email dan false password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>97f5cd2d-50a8-4643-9693-350be6548e75</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Page/Success login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>104b631c-012f-47af-b70d-491f52761f91</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HomePage/Success fitering product Z to A</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e169e955-41d5-4deb-8569-0973bf473657</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HomePage/Success change view product in homepage</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8b0e2234-43ae-4b6a-abe7-b6cf64f1ce45</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HomePage/Success fitering product price high to low</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e42db12f-1eba-4467-9917-84a87d24a0e9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HomePage/Success fitering product A to Z</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dbda5053-5f03-4752-bb38-1b4d543bfeca</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User Order/Success buy 1 product</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
